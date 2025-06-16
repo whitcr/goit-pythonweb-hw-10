330362e49f3d9e1921063f52f58226859e1029d9
Denis Lienskyi <whitcr0k@gmail.com>
2025-06-16 11:49:54 +0200

    fix

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/contactsdb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
